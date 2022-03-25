d04abb2f6bcb1b9c47923862ef6710a2f27ad241
asdf <asdf@gmail.com>
2022-03-25 20:19:06 +0900

    [김인규]  - 배포용 코드 수정

--- src/main/java/com/marondal/marondalgram/common/FileManagerService.java
+++ src/main/java/com/marondal/marondalgram/common/FileManagerService.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileManagerService {
 	
-	public final static String FILE_UPLOAD_PATH = "D:\\김인규 강사\\web\\0927\\springProject\\upload\\image/";
+	public final static String FILE_UPLOAD_PATH = "/home/ec2-user/marondalgram/upload/images/";
 	
 	private static Logger logger = LoggerFactory.getLogger(FileManagerService.class);
 
